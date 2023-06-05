d51d19b737f5c329d9b03f21a7f8fd63b8b794c5
JaviLoopZ <strangemitico15@gmail.com>
2023-06-04 23:13:37 -0700

    Se agregan cambios en footer y product page. Se agregó una página About.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,24 @@
 import React from 'react';
+import AboutPage from '../pages/AboutPage'
+import ContactPage from '../pages/ContactPage';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 export const Footer = () => {
+
+    const footerLinksStyle = {
+        display: 'flex',
+        justifyContent: 'center',
+        marginTop: '1rem',
+        fontFamily: 'monospace'
+    };
+
+    const footerLinkStyle = {
+        color: 'white',
+        marginLeft: '1rem',
+        textDecoration: 'none',
+    };
+
     return (
         <footer style={{ 
             backgroundColor: '#1976d2', 
@@ -13,7 +31,13 @@ export const Footer = () => {
             width: '100%',
             boxShadow: '0px 2px 4px -1px rgba(0,0,0,0.2), 0px 4px 5px 0px rgba(0,0,0,0.14), 0px 1px 10px 0px rgba(0,0,0,0.12)'            
             }}>
+            
             <div className="footer-container">
+                <div className="footer-links" style={footerLinksStyle}>
+                    <Link to="/contact" style={footerLinkStyle} className="footer-link">Contacto</Link>
+                    <Link to="/about" style={footerLinkStyle}className="footer-link">Acerca de</Link>
+                </div>
+
                 <p className="footer-text">© 2023 Shopping. Todos los derechos reservados.</p>
             </div>
         </footer>
--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useState } from 'react';
 
 export const ProductPage = () => {
-    
-    return (<div>ProductPage</div>)
+
+    const [item, setItem] = useState({
+        name: 'Nombre del artículo',
+        price: 9.99,
+        description: 'Descripción del artículo',
+    });
+
+    const addToCart = () => {
+        console.log('Artículo agregado al carrito');
+    };
+
+    return (<div>
+
+        <h2>{item.name}</h2>
+        <p>Precio: ${item.price}</p>
+        <p>{item.description}</p>
+        <button onClick={addToCart}>Agregar al carrito</button>
+    </div>
+        
+    )
 }
 
 export default ProductPage
